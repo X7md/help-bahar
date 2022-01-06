f51865cef5961708af172cf55fd3895565cd83ea
qtrhfem7et <87308757+qtrhfem7et@users.noreply.github.com>
2022-01-06 16:04:58 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+class Tests extends Component {
+  state = { showing: true };
+
+  render() {
+    
+    return (
+      
+
+        <div className="b-border">
+          <div className="logo"></div>
+          <div className="s-border">
+            
+            <div className="hi">
+                
+                  <h3 className="jdwl"> الجدول </h3>
+                  <h3 className="best"> لوحة الشرف </h3>
+                  <h3 className="rules"> القوانين </h3>
+                  <h3 className="apptow"> البرامج </h3>
+
+                
+              </div>
+          </div>
+        </div>
+      
+    );
+  }
+}
+
+export default Tests;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Tests from './App';
+
+
+ReactDOM.render(
+  
+    <Tests />,document.getElementById('root')
+);
+
